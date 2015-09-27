754dd024796325ebdcb2a51a7412b8fafe9cdfb6
Felipe Román <froman10@alumnos.utalca.cl>
2015-09-27 12:40:17 -0300

    modelo-usuario
    se crea la clase abstracta usuario con métodos virtual

--- Taller.Estacionamiento/Models/Usuario.cs
+++ Taller.Estacionamiento/Models/Usuario.cs
@@ -5,7 +5,33 @@ using System.Web;
 
 namespace Taller.Estacionamiento.Models
 {
-    public class Usuario
+    public abstract class Usuario
     {
+        public string Nombre { get; set; }
+        public int Rut{get; set;}
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+        public int Telefono { get; set; }
+
+        public virtual bool IniciarSesion(string email, string contraseña)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual bool CerrarSesion(string email)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual void Agregar()
+        {
+            throw new NotImplementedException();
+        }
+        public virtual void Modificar()
+        {
+            throw new NotImplementedException();
+        }
+        public virtual void Eliminar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
