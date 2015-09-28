00d2311d66e090aa80cfa5812dfb8c2dddf52088
jorge <darkpinguino@gmail.com>
2015-09-28 17:33:42 -0300

    implementación tarjetero
    agregado el modelo y los métodos del tarjetero (Tarjetero.cs)

---
+++ Taller.Estacionamiento/Models/Tarjetero.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Taller.Estacionamiento.Models
+{
+    public class Tarjetero
+    {
+        public bool registarIngreso(Personal personal)
+        {
+            throw new NotImplementedException();
+        }
+        public bool registrarSalida(Personal personal)
+        {
+            throw new NotImplementedException();
+        }
+        public bool eliminarRegistroPersonal(Personal personal)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Personal> personalTrabajando()
+        {
+            throw new NotImplementedException();
+        }
+        public List<DateTime> entradasPersonal(Personal personal)
+        {
+            throw new NotImplementedException();
+        }
+        public List<DateTime> salidasPersonal(Personal personal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
