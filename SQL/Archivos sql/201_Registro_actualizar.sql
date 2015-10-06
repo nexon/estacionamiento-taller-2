98cde9bad8b11a24f323f0c703ed240815b86954
Felipe Román <froman10@alumnos.utalca.cl>
2015-10-06 01:39:59 -0300

    Corrección de SP de tabla registro
    Se realizan algunas correcciones a los SPs de la tabla registro.

--- SQL/Archivos sql/201_Registro_actualizar.sql
+++ SQL/Archivos sql/201_Registro_actualizar.sql
@@ -1,11 +1,13 @@
 
 DROP PROCEDURE IF EXISTS registro_actualizar$
-CREATE PROCEDURE registro_actualizar(in id_registroIn INT, in fecha_salidaIn DATETIME, in monto_totalIn INT)
+CREATE PROCEDURE registro_actualizar(in id_registroIn INT, in fecha_ingresoIn INT, fecha_salidaIn DATETIME, in monto_totalIn INT)
 BEGIN
 	update registro
 	set
+		fecha_ingreso = fecha_ingresoIn,
 		fecha_salida = fecha_salidaIn, 
 		monto_total = monto_totalIn
-	where id_registro = id_registroIn;
+	where 
+		id_registro = id_registroIn;
 
 END
--- SQL/Archivos sql/202_Registro_eliminar.sql
+++ SQL/Archivos sql/202_Registro_eliminar.sql
@@ -2,6 +2,9 @@
 DROP PROCEDURE IF EXISTS registro_eliminar$
 CREATE PROCEDURE registro_eliminar(in id_registroIn INT)
 BEGIN
-	delete from registro where id_registro = id_registroIn;
+	delete from
+		registro
+	where 
+		id_registro = id_registroIn;
 
 END
