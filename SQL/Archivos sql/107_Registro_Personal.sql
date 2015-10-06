6cc66a2b43bd5d335cd1b0f1b92b4a6de33c7044
Maximiliano <mverdugo10@alumnos.utalca.cl>
2015-10-06 18:10:09 +0200

    Update de Tablas
    Corregidos errores con id_personal

--- SQL/Archivos sql/107_Registro_Personal.sql
+++ SQL/Archivos sql/107_Registro_Personal.sql
@@ -3,11 +3,11 @@ CREATE TABLE registro_personal
 (
  id_registro_personal INT NOT NULL AUTO_INCREMENT,
  fecha_ingreso DATETIME NOT NULL,
- fecha_salida DATETIME NOT NULL,
+ fecha_salida DATETIME,
  estacionamiento_id_estacionamiento INT NOT NULL,
  personal_id_empleado INT NOT NULL,
  PRIMARY KEY (id_registro_personal),
  FOREIGN KEY (estacionamiento_id_estacionamiento) REFERENCES estacionamiento (id_estacionamiento),
- FOREIGN KEY (personal_id_empleado) REFERENCES personal (id_empleado)
+ FOREIGN KEY (personal_id_empleado) REFERENCES personal (id_personal)
 )
 $$
--- SQL/Archivos sql/108_Personal_Estacionamiento.sql
+++ SQL/Archivos sql/108_Personal_Estacionamiento.sql
@@ -4,8 +4,8 @@ CREATE TABLE personal_estacionamiento
  id INT NOT NULL AUTO_INCREMENT,
  personal_id_empleado INT NOT NULL,
  estacionamiento_id_estacionamiento INT NOT NULL,
- PRIMARY KEY (id_registro_personal),
+ PRIMARY KEY (id),
  FOREIGN KEY (estacionamiento_id_estacionamiento) REFERENCES estacionamiento (id_estacionamiento),
- FOREIGN KEY (personal_id_empleado) REFERENCES personal (id_empleado)
+ FOREIGN KEY (personal_id_empleado) REFERENCES personal (id_personal)
 )
 $$
