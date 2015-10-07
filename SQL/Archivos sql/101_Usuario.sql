ed7b6d79eb45bf2182217fb5f6a948496b71d99c
Alberto Lagos Toro <alberto.lagos@gmail.com>
2015-10-07 08:36:36 -0400

    Revert "cambios en los archivos BD"
    This reverts commit 880951e0faba5846105eedb60090ec45768d0b96.

--- SQL/Archivos sql/101_Usuario.sql
+++ SQL/Archivos sql/101_Usuario.sql
@@ -3,10 +3,8 @@ CREATE TABLE usuario
 (
  id_usuario INT NOT NULL AUTO_INCREMENT,
  nombre VARCHAR(45) NOT NULL,
- rut INT,
+ rut VARCHAR(15),
  contrasenia VARCHAR(45) NOT NULL,
- email VARCHAR(45) NOT NULL,
- telefono INT,
  PRIMARY KEY (id_usuario)
 )
 $$
--- SQL/Archivos sql/103_Personal.sql
+++ SQL/Archivos sql/103_Personal.sql
@@ -3,7 +3,10 @@ CREATE TABLE personal
 (  
  id_personal INT NOT NULL AUTO_INCREMENT,
  id_usuario INT NOT NULL,
+ id_estacionamiento INT NOT NULL,
+ propietario BOOLEAN NOT NULL,
  PRIMARY KEY (id_personal),
- FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
+ FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario),
+ FOREIGN KEY (id_estacionamiento) REFERENCES estacionamiento (id_estacionamiento)
 )
 $$
