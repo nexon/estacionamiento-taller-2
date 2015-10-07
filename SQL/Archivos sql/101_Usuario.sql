a030a20089e6b308f8062411e6fa36fb986a3b5f
Alberto Lagos Toro <alberto.lagos@gmail.com>
2015-10-07 09:53:14 -0400

    Cambios que hizo la Yanira en develop
    Además cambie el orden de ejecución de Registro y Espacio (ahora se
    ejecuta primero Espacio y Registro

--- SQL/Archivos sql/101_Usuario.sql
+++ SQL/Archivos sql/101_Usuario.sql
@@ -3,7 +3,7 @@ CREATE TABLE usuario
 (
  id_usuario INT NOT NULL AUTO_INCREMENT,
  nombre VARCHAR(45) NOT NULL,
- rut VARCHAR(15),
+ rut INT,
  contrasenia VARCHAR(45) NOT NULL,
  PRIMARY KEY (id_usuario)
 )
