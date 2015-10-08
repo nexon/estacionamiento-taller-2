d38deecaad796c42105b2e726dab252ac16419d1
AnaMariaElizabeth <anymaria7@gmail.com>
2015-10-08 20:45:30 -0300

    Modificaciones a CRUD Conductor
    Crear
    Seleccionar
    Modificar
    Eliminar

--- SQL/Archivos sql/202_conductor_Seleccionar.sql
+++ SQL/Archivos sql/202_conductor_Seleccionar.sql
@@ -1,11 +1,11 @@
-DROP PROCEDURE IF EXISTS conductor_Seleccionar $$
-CREATE PROCEDURE conductor_Seleccionar
+DROP PROCEDURE IF EXISTS Conductor_Seleccionar $$
+CREATE PROCEDURE Conductor_Seleccionar
 (
-	IN inId_conductor INT
+	inId_conductor INT
 ) 
 BEGIN
 SELECT
-	id_conductor,id_usuario,reputacion
+	id_conductor,id_usuario
 FROM conductor
 WHERE id_conductor=inId_conductor;
 END$$
