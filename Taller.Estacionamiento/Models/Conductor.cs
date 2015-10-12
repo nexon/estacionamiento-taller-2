adc2c4954c4ab6e0a96fb1303c96c6d14c5d6c2b
amiranda10 <amiranda10@alumnos.utalca.cl>
2015-10-12 01:24:17 -0300

    métodos agregar valoración
    Se agregaron lo métodos para agregar valoración tanto en conductor como
    en estacionamiento

--- Taller.Estacionamiento/Models/Conductor.cs
+++ Taller.Estacionamiento/Models/Conductor.cs
@@ -9,9 +9,88 @@ namespace Taller.Estacionamiento.Models
 {
     public class Conductor:Usuario
     {
-        public void AgregarValoracion(double valoracion)
+        public void AgregarValoracion(double valoracion, int idUsuario, int idEstacionamiento)
         {
-            throw new NotImplementedException();
+            var dt = new DataTable();
+            var command = new MySqlCommand() { CommandType = CommandType.StoredProcedure, CommandText = "valoracion_seleccionar" };
+            command.Parameters.AddWithValue("inIdUsuario", idUsuario);
+            command.Parameters.AddWithValue("inIdEstacionamiento", idEstacionamiento);
+            using (var conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                conn.Open();
+                command.Connection = conn;
+                var sqlda = new MySqlDataAdapter(command);
+                sqlda.Fill(dt);
+                conn.Close();
+            }
+            if (dt.Rows.Count > 0)
+            {
+                //si se obtiene una fila como respuesta es por que hay una valoracion existente que asocia el conductor y el estacionamiento
+                DataRow dr = dt.Rows[0];
+                int idValoracion = Convert.ToInt32(dr["valoracion_id_valoracion"]);
+                float valoracionConductor = (float)dr["valoracion_valor_conductor"];
+                float valoracionEstacionamiento = (float)dr["valoracion_valor_estacionamiento"];
+                if (valoracionConductor == 0)//como la valoracion es 0 significa que no esta hecha por tanto se modifica
+                {
+                    /*se crea el comando, luego se usa una segunda coneccion para enviar los datos*/
+                    var commandM = new MySqlCommand() { CommandType = CommandType.StoredProcedure, CommandText = "valoracion_modificar" };
+                    commandM.Parameters.AddWithValue("inIdValoracion", idValoracion);
+                    commandM.Parameters.AddWithValue("inValorConductor", (float)valoracion);
+                    commandM.Parameters.AddWithValue("inValorEstacionamiento", valoracionEstacionamiento);
+                    using (var conn2 = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                    {
+
+                        conn2.Open();
+                        var sqlTran = conn2.BeginTransaction();
+                        try
+                        {
+                            commandM.Connection = conn2;
+                            commandM.Transaction = sqlTran;
+                            commandM.ExecuteNonQuery();
+                            sqlTran.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            sqlTran.Rollback();
+                            throw ex;
+                        }
+                        finally
+                        {
+                            conn2.Close();
+                        }
+                    }
+                    return;//como ya se modifico la fila termino la funcion aqui
+                }
+            }
+            //si la valoracion es mayor a 0 o no existe significa que la valoracion que se necesita crear una nueva
+            /*se crea el comando, luego se usa una segunda coneccion para enviar los datos*/
+            var commandC = new MySqlCommand() { CommandType = CommandType.StoredProcedure, CommandText = "valoracion_crear" };
+            commandC.Parameters.AddWithValue("inIdUsuario", idUsuario);
+            commandC.Parameters.AddWithValue("inIdEstacionamiento", idEstacionamiento);
+            commandC.Parameters.AddWithValue("inValorConductor", (float)valoracion);
+            commandC.Parameters.AddWithValue("inValorEstacionamiento", 0);
+            using (var conn2 = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+
+                conn2.Open();
+                var sqlTran = conn2.BeginTransaction();
+                try
+                {
+                    commandC.Connection = conn2;
+                    commandC.Transaction = sqlTran;
+                    commandC.ExecuteNonQuery();
+                    sqlTran.Commit();
+                }
+                catch (Exception ex)
+                {
+                    sqlTran.Rollback();
+                    throw ex;
+                }
+                finally
+                {
+                    conn2.Close();
+                }
+            }
         }
 
         double PromedioValoraciones(int idUsuario)
--- Taller.Estacionamiento/Models/Estacionamiento.cs
+++ Taller.Estacionamiento/Models/Estacionamiento.cs
@@ -70,9 +70,88 @@ namespace Taller.Estacionamiento.Models
             throw new NotImplementedException();
         }
 
-        void AgregarValoracion(double Valoracion)
+        public void AgregarValoracion(double valoracion, int idUsuario, int idEstacionamiento)
         {
-            throw new NotImplementedException();
+            var dt = new DataTable();
+            var command = new MySqlCommand() { CommandType = CommandType.StoredProcedure, CommandText = "valoracion_seleccionar" };
+            command.Parameters.AddWithValue("inIdUsuario", idUsuario);
+            command.Parameters.AddWithValue("inIdEstacionamiento", idEstacionamiento);
+            using (var conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                conn.Open();
+                command.Connection = conn;
+                var sqlda = new MySqlDataAdapter(command);
+                sqlda.Fill(dt);
+                conn.Close();
+            }
+            if (dt.Rows.Count > 0)
+            {
+                //si se obtiene una fila como respuesta es por que hay una valoracion existente que asocia el conductor y el estacionamiento
+                DataRow dr = dt.Rows[0];
+                int idValoracion = Convert.ToInt32(dr["valoracion_id_valoracion"]);
+                float valoracionConductor = (float)dr["valoracion_valor_conductor"];
+                float valoracionEstacionamiento = (float)dr["valoracion_valor_estacionamiento"];
+                if (valoracionEstacionamiento == 0)//como la valoracion es 0 significa que no esta hecha por tanto se modifica
+                {
+                    /*se crea el comando, luego se usa una segunda coneccion para enviar los datos*/
+                    var commandM = new MySqlCommand() { CommandType = CommandType.StoredProcedure, CommandText = "valoracion_modificar" };
+                    commandM.Parameters.AddWithValue("inIdValoracion", idValoracion);
+                    commandM.Parameters.AddWithValue("inValorConductor", valoracionConductor);
+                    commandM.Parameters.AddWithValue("inValorEstacionamiento", (float)valoracion);
+                    using (var conn2 = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                    {
+
+                        conn2.Open();
+                        var sqlTran = conn2.BeginTransaction();
+                        try
+                        {
+                            commandM.Connection = conn2;
+                            commandM.Transaction = sqlTran;
+                            commandM.ExecuteNonQuery();
+                            sqlTran.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            sqlTran.Rollback();
+                            throw ex;
+                        }
+                        finally
+                        {
+                            conn2.Close();
+                        }
+                    }
+                    return;//como ya se modifico la fila termino la funcion aqui
+                }
+            }
+            //si la valoracion es mayor a 0 o no existe significa que la valoracion que se necesita crear una nueva
+            /*se crea el comando, luego se usa una segunda coneccion para enviar los datos*/
+            var commandC = new MySqlCommand() { CommandType = CommandType.StoredProcedure, CommandText = "valoracion_crear" };
+            commandC.Parameters.AddWithValue("inIdUsuario", idUsuario);
+            commandC.Parameters.AddWithValue("inIdEstacionamiento", idEstacionamiento);
+            commandC.Parameters.AddWithValue("inValorConductor", 0);
+            commandC.Parameters.AddWithValue("inValorEstacionamiento", (float)valoracion);
+            using (var conn2 = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+
+                conn2.Open();
+                var sqlTran = conn2.BeginTransaction();
+                try
+                {
+                    commandC.Connection = conn2;
+                    commandC.Transaction = sqlTran;
+                    commandC.ExecuteNonQuery();
+                    sqlTran.Commit();
+                }
+                catch (Exception ex)
+                {
+                    sqlTran.Rollback();
+                    throw ex;
+                }
+                finally
+                {
+                    conn2.Close();
+                }
+            }
         }
 
         double PromedioValoraciones(int idEstacionamiento)
