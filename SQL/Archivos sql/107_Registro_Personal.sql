4c762be8bf7717e64060fe6d7ca442f96ea5cb83
Maximiliano <mverdugo10@alumnos.utalca.cl>
2015-10-13 20:13:28 +0200

    Correción Nonbres Tabla Registro_Personal

--- SQL/Archivos sql/107_Registro_Personal.sql
+++ SQL/Archivos sql/107_Registro_Personal.sql
@@ -4,10 +4,10 @@ CREATE TABLE registro_personal
  id_registro_personal INT NOT NULL AUTO_INCREMENT,
  fecha_ingreso DATETIME NOT NULL,
  fecha_salida DATETIME,
- estacionamiento_id_estacionamiento INT NOT NULL,
- personal_id_empleado INT NOT NULL,
+ id_estacionamiento INT NOT NULL,
+ id_personal INT NOT NULL,
  PRIMARY KEY (id_registro_personal),
- FOREIGN KEY (estacionamiento_id_estacionamiento) REFERENCES estacionamiento (id_estacionamiento),
- FOREIGN KEY (personal_id_empleado) REFERENCES personal (id_personal)
+ FOREIGN KEY (id_estacionamiento) REFERENCES estacionamiento (id_estacionamiento),
+ FOREIGN KEY (id_personal) REFERENCES personal (id_personal)
 )
 $$
