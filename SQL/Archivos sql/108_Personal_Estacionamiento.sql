19d10cba8e474826db16b45e90b655587e031fcf
Maximiliano <mverdugo10@alumnos.utalca.cl>
2015-10-13 20:09:11 +0200

    Correción Tabla Personal_Estacionamiento
    Cambio de nombre de los campos según lo q aparece en la Base de datos

--- SQL/Archivos sql/108_Personal_Estacionamiento.sql
+++ SQL/Archivos sql/108_Personal_Estacionamiento.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS personal_estacionamiento $$
 CREATE TABLE personal_estacionamiento
 (
  id INT NOT NULL AUTO_INCREMENT,
- personal_id_empleado INT NOT NULL,
- estacionamiento_id_estacionamiento INT NOT NULL,
+ id_personal INT NOT NULL,
+ id_estacionamiento INT NOT NULL,
  PRIMARY KEY (id),
- FOREIGN KEY (estacionamiento_id_estacionamiento) REFERENCES estacionamiento (id_estacionamiento),
- FOREIGN KEY (personal_id_empleado) REFERENCES personal (id_personal)
+ FOREIGN KEY (id_estacionamiento) REFERENCES estacionamiento (id_estacionamiento),
+ FOREIGN KEY (id_personal) REFERENCES personal (id_personal)
 )
 $$
