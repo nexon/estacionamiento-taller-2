140e5801545d222c887a515430366ca89d500a70
AnaMariaElizabeth <anymaria7@gmail.com>
2015-10-15 01:35:20 -0300

    Modificaciones PK
    Se modificaron las PK y se agrego el UNIQUE en Conductor para la
    relacion 1-1

--- SQL/Archivos sql/201_Conductor_Crear.sql
+++ SQL/Archivos sql/201_Conductor_Crear.sql
@@ -1,12 +1,9 @@
  DROP PROCEDURE IF EXISTS Conductor_Crear $$
  CREATE PROCEDURE Conductor_Crear
  (
- 	IN inId_usuario INT
- )  
-IF(SELECT id_usuario FROM conductor WHERE id_usuario = inId_usuario) THEN
-    SELECT id_usuario from conductor where id_usuario = inId_usuario;
-ELSE
-INSERT INTO conductor(id_usuario)
- 	VALUES ((SELECT id_usuario FROM usuario Where id_usuario=inId_usuario));
-END IF;
+ 	inId_usuario INT
+ ) 
+ INSERT INTO conductor(id_usuario)
+ 	VALUES ((SELECT rut FROM usuario Where rut=inId_usuario));
+ 	
 $$
