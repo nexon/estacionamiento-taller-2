8ea1ebd1a8f72e98352a684052351632cbbbf5a1
AnaMariaElizabeth <anymaria7@gmail.com>
2015-10-15 00:14:57 -0300

    Modificacion del PK
    Se realizaron cambios en las tablas Usuario, Conductor y Personal
    
    Se modifico el nombre de Actualizar a Modificar

--- SQL/Archivos sql/102_Conductor.sql
+++ SQL/Archivos sql/102_Conductor.sql
@@ -4,6 +4,6 @@ CREATE TABLE conductor
  id_conductor INT NOT NULL AUTO_INCREMENT,
  id_usuario INT NOT NULL,
  PRIMARY KEY (id_conductor),
- FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
+ FOREIGN KEY (id_usuario) REFERENCES usuario (rut)
 )
 $$
