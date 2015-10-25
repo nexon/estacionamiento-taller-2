9da74adfc02af84c1a89a663a10ee357efeebca6
AnaMariaElizabeth <anymaria7@gmail.com>
2015-10-25 17:26:20 -0300

    Modificacion por test

--- SQL/Archivos sql/406_Estacionamiento_Eliminar_Personal.sql
+++ SQL/Archivos sql/406_Estacionamiento_Eliminar_Personal.sql
@@ -7,15 +7,15 @@ CREATE PROCEDURE Estacionamiento_Eliminar_Personal (
 
 BEGIN 
 	DELETE personal_estacionamiento
-	From personal_estacionamiento INNER JOIN personal
-	on personal_estacionamiento.id_personal = personal.id_personal
+	FROM personal_estacionamiento LEFT JOIN personal
+	ON personal_estacionamiento.id_personal = personal.id_personal
     WHERE 
     personal_estacionamiento.id_estacionamiento  =   inID_Estacionamiento AND 
     personal_estacionamiento.id_personal  
     IN 
-    (select id_personal  
-     from personal
-     where id_usuario = inID_RUT ); 
+    (SELECT id_personal  
+     FROM personal
+     WHERE id_usuario = inID_RUT ); 
 
 END
 $$
