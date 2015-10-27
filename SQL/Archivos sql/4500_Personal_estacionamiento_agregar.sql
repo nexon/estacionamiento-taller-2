865122a45e5d732c488de9f0c9f08a9ee257d52c
jorge <darkpinguino@gmail.com>
2015-10-27 00:20:45 -0300

    editar tabla personal_estacionamiento
    se quieto el id autoincrement y se dejo como pk el id_personal
    id_estacionamiento y el id_rol.
    se modifico el procedieminto almacenado
    Personal_esctacionamiento_agregar

--- SQL/Archivos sql/4500_Personal_estacionamiento_agregar.sql
+++ SQL/Archivos sql/4500_Personal_estacionamiento_agregar.sql
@@ -1,10 +1,14 @@
 DROP PROCEDURE IF EXISTS personal_estacionamiento_agregar $$
-CREATE PROCEDURE personal_estacionamiento_agregar(in id_personalIn INT, in id_estacionamientoIn INT)
+CREATE PROCEDURE personal_estacionamiento_agregar(
+	inId_personal INT, 
+	inId_estacionamientoIn INT,
+	inId_rol
+)
 BEGIN
 	insert into 
-		personal_estacionamiento(id_personal, id_estacionamiento)
+		personal_estacionamiento(id_personal, id_estacionamiento, id_rol)
 	values
-		(id_personalIn, id_estacionamientoIn);
+		(inId_personal, inId_estacionamiento, inId_rol);
 
 END
 $$
