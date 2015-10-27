3a0c3adb5aa8f2fbd9ca76c46fc4beeb70ed3ecd
jorge <darkpinguino@gmail.com>
2015-10-27 01:09:04 -0300

    modificar personal
    editado el SP usuario_modificar con los parametros correspondientes,
    creado el SP personal_estacionamiento_modificar.
    creados los metodos modificar en los modelos usuario y personal
    agregado atributo id en personal

---
+++ SQL/Archivos sql/406_Usuario_Modificar.sql
@@ -0,0 +1,18 @@
+DROP PROCEDURE IF EXISTS usuario_modificar $$
+CREATE PROCEDURE usuario_modificar(
+	inRut INT,
+    inNombre VARCHAR(45),
+	inContrasenia VARCHAR(45),
+    inEmail VARCHAR(45),
+    inTelefono INT,
+)
+BEGIN
+	UPDATE usuario
+	SET 
+		nombre = inNombre,
+        contrasenia = inContrasenia,
+        email = inEmail,
+        inTelefono = inTelefono
+        
+	WHERE rut = inRut;
+END $$
---
+++ SQL/Archivos sql/4501_Personal_estacionamiento_modificar.sql
@@ -0,0 +1,13 @@
+DROP PROCEDURE IF EXISTS personal_estacionamiento_modificar $$
+CREATE PROCEDURE personal_estacionamiento_modificar(
+	inIdPersonal INT,
+    inIdEstacionamiento INT,
+	inIdRol INT,
+)
+BEGIN
+	UPDATE personal_estacionamiento
+	SET 
+		rol = inIdRol
+        
+	WHERE id_personal = inIdPersonal AND id_estacionamiento = inIdEstacionamiento AND id_rol = inIdRol;
+END $$
