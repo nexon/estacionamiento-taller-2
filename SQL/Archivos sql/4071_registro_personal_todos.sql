86260ed4d22f1ee9891d5475bf80ee869b1f4091
Patricio Pino <papsalazar@hotmail.com>
2015-11-04 01:19:50 -0300

    Conectar Vista Tarjetero
    Se agregaron los archivos sql necesarios para traer los datos desde la
    base de datos.
    Se editó la vista Tarjetero.
    Se el método Tarjetero en el controlador Estacionamiento.

---
+++ SQL/Archivos sql/4071_registro_personal_todos.sql
@@ -0,0 +1,26 @@
+DROP PROCEDURE IF EXISTS registro_personal_todos $$
+CREATE PROCEDURE registro_personal_todos(
+	inIdEstacionamiento INT(11)
+) 
+BEGIN
+  SELECT
+	u.nombre, 
+    u.rut,
+    rp.fecha_ingreso,
+    rp.fecha_salida
+  FROM 
+	(SELECT
+		r.fecha_ingreso,
+        r.fecha_salida,
+        r.id_estacionamiento,
+        p.id_usuario
+	 FROM
+		registro_personal r
+	 INNER JOIN personal p
+     ON r.id_personal = p.id_personal
+    ) as rp
+  INNER JOIN
+	usuario u
+  ON
+	 rp.id_usuario = u.rut and rp.id_estacionamiento = inIdEstacionamiento;
+END $$
