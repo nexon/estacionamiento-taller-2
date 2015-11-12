c797fbadaa16dd10b7e5605a0e0372ce9f3b7ea6
eortizutalca <eortiz@utalca.cl>
2015-11-12 15:17:17 -0300

    Agrega clase SessionManager
    Se agrega la clase SessionManager que guardará y entregará el usuario
    autentificado y el estacionamiento seleccionado por el usuario.

---
+++ Taller.Estacionamiento/Utils/SessionManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Taller.Estacionamiento
+{
+    public class SessionManager
+    {
+        /// <summary>
+        /// Retorna el usuario que esta autentificado
+        /// </summary>
+        /// <returns>El usuario autentificado, null en caso contrario</returns>
+        public static Models.Usuario UsuarioAutenticado()
+        {
+            var usuario = new Models.Usuario();
+            if (HttpContext.Current.Session["USUARIO_AUTENTIFICADO"] != null)
+            {
+                usuario = (Models.Usuario)HttpContext.Current.Session["USUARIO_AUTENTIFICADO"];
+            }
+            else
+            {
+                usuario = null;
+            }
+            return usuario;
+        }
+
+        /// <summary>
+        /// Establece el usuario autenticado en una variable de sesion
+        /// </summary>
+        /// <param name="usuario">El usuario que se quiere permanecer autentificado</param>
+        public static void ModificarUsuarioAutenticado(Models.Usuario usuario)
+        {
+            if(usuario != null)
+            {
+                HttpContext.Current.Session.Add("USUARIO_AUTENTIFICADO", usuario);
+            }
+            else
+            {
+                HttpContext.Current.Session.Remove("USUARIO_AUTENTIFICADO");
+            }
+        }
+
+
+        /// <summary>
+        /// Retorna el estacionamiento que esta siendo usado por el usuario
+        /// </summary>
+        /// <returns>El estacionamiento en uso, null en caso contrario</returns>
+        public static Models.Estacionamiento EstacionamientoSeleccionado()
+        {
+            var estacionamiento = new Models.Estacionamiento();
+            if (HttpContext.Current.Session["ESTACIONAMIENTO_ID"] != null)
+            {
+                if(!estacionamiento.Seleccionar(Convert.ToInt32(HttpContext.Current.Session["ESTACIONAMIENTO_ID"])))
+                {
+                    estacionamiento = null;
+                }
+            }
+            else
+            {
+                estacionamiento = null;
+            }
+            return estacionamiento;
+        }
+
+        /// <summary>
+        /// Establece el estacionamiento en uso en una variable de sesion
+        /// </summary>
+        /// <param name="estacionamiento">El estacionamiento que se quiere permanecer en uso</param>
+        public static void ModificarEstacionamientoSeleccionado(Models.Estacionamiento estacionamiento)
+        {
+            if (estacionamiento != null)
+            {
+                HttpContext.Current.Session.Add("ESTACIONAMIENTO_ID", estacionamiento.ID);
+            }
+            else
+            {
+                HttpContext.Current.Session.Remove("ESTACIONAMIENTO_ID");
+            }
+            
+        }
+    }
+}
