d8cc2b9a5399fd19317c921689f4d15f718af631
jorge <darkpinguino@gmail.com>
2015-11-13 01:34:51 -0300

    obtener ID de estacionamiento desde SessionManager
    modificado el namespace de SessionManager, a
    "Taller.Estacionamiento.Utils"
    obtener ID en _Layout desde SessionManager en la referencia a tarifas

--- Taller.Estacionamiento/Utils/SessionManager.cs
+++ Taller.Estacionamiento/Utils/SessionManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Taller.Estacionamiento
+namespace Taller.Estacionamiento.Utils
 {
     public class SessionManager
     {
