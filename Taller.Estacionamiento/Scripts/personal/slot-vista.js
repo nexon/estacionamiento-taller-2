ac8856a2b4083aa0f2942d925099dcfb36120aa3
AnaMariaElizabeth <anymaria7@gmail.com>
2015-11-14 16:38:38 -0300

    Modificaciones
    Agregar con modal las vistas

---
+++ Taller.Estacionamiento/Scripts/personal/slot-vista.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+
+    // se selecciona el boton editar de cada personal
+    $(".slot-editar-eliminar").find(".green").on("click", function (e) {
+
+        var codigo = $(this).attr("data-codigo");
+        var estado = $(this).attr("data-estado");
+
+        $("#slot-editar-codigo").val(codigo);
+        $("#slot-editar-estado").val(estado);
+
+        $(this).attr("#modalEditarSlot").dialog("open");
+
+        return false;
+    });
+
+
+    // se selecciona el boton eliminar de cada personal
+    $(".slot-editar-eliminar").find(".red").on("click", function (e) {
+
+        var codigo = $(this).attr("data-codigo");
+        var estado = $(this).attr("data-estado");
+
+        $("#slot-eliminar-codigo").val(codigo);
+        $("#slot-eliminar-estado").val(estado);
+
+        /* -cada atributo tiene un campo hidden
+          -los atributos se deben modificar para ser enviado por post cuando se elimine un personal
+       */
+        $("#slot-eliminar-codigo-hidden").val(codigo);
+        $("#slot-eliminar-estado-hidden").val(estado);
+
+        $(this).attr("#modalEliminarSlot").dialog("open");
+
+        return false;
+
+
+    });
+
+
+
+});
