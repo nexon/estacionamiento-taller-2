d3a24c52462fdf668fc5999e2ddb15a16f303bcb
juagonzalez <juagonzalez09@alumnos.utalca.cl>
2015-11-21 23:41:42 -0300

    Vehiculo_Agregar
    SP para agregar un vehiculo

--- SQL/Archivos sql/41104_Vehiculo_Agregar.sql
+++ SQL/Archivos sql/41104_Vehiculo_Agregar.sql
@@ -1,18 +1,18 @@
 DROP PROCEDURE IF EXISTS Vehiculo_Agregar $$
-CREATE PROCEDURE Vehiculo_Agregar(
-    inPatente varchar(15),
-    IN inIDCOnductor int
-)
-BEGIN
-	INSERT INTO Vehiculo
-		(
-			patente,
-			id_conductor
-		)
-	VALUES
-		(
-			inPatente,
-			inIDCOnductor
-		);
-END
+	CREATE PROCEDURE Vehiculo_Agregar(
+	    inPatente varchar(15),
+	    IN inIDCOnductor int
+	)
+	BEGIN
+		INSERT INTO Vehiculo
+			(
+				patente,
+				id_conductor
+			)
+		VALUES
+			(
+				inPatente,
+				inIDCOnductor
+			);
+	END
 $$
